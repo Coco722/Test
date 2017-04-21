745e6a503a09457372f67c13fcf61770dc1f1b8d
cthebaudin <corentin.thebaudin@laposte.net>
2017-04-21 09:58:26 +0200

    corr

--- HelloWorld.java
+++ HelloWorld.java
@@ -20,7 +20,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        ystem.out.println("WoW");
+        System.out.println("WoW");
     }
 
 }
